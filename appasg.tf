9ab0dfb0a4d23e77c9e5277de8fcceba8aea9334
psameerkhan112 <psameerkhan112@gmail.com>
2025-09-16 12:45:48 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -21,7 +21,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-0b982602dbb32c5bd"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "devopsbyraham"
 
   network_interfaces {
