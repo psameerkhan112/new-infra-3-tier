7515ea2af0515bbbce013a2391c441e4b09d40a1
psameerkhan112 <psameerkhan112@gmail.com>
2025-09-16 12:45:29 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -14,8 +14,8 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 ###### Create a Launch Template for the EC2 instances ######
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
-  image_id      = "ami-0ed194d7eff6d2f81"
-  instance_type = "t2.micro"
+  image_id      = "ami-0b982602dbb32c5bd"
+  instance_type = "t3.micro"
   key_name      = "devopsbyraham"
   network_interfaces {
     associate_public_ip_address = true
